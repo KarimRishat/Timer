9daf175ab4dd5cb057938cc00fe1dc26ac80d67b
Rishat <karimr2000@mail.ru>
2022-05-20 20:54:35 +0300

    Добавьте файлы проекта.

---
+++ timer/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CancelBut = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.secLabel = new System.Windows.Forms.Label();
+            this.minLabel = new System.Windows.Forms.Label();
+            this.hoursLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.hLabel = new System.Windows.Forms.Label();
+            this.mLabel = new System.Windows.Forms.Label();
+            this.slabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CancelBut
+            // 
+            this.CancelBut.Enabled = false;
+            this.CancelBut.Location = new System.Drawing.Point(120, 347);
+            this.CancelBut.Name = "CancelBut";
+            this.CancelBut.Size = new System.Drawing.Size(75, 23);
+            this.CancelBut.TabIndex = 1;
+            this.CancelBut.Text = "cancel";
+            this.CancelBut.UseVisualStyleBackColor = true;
+            this.CancelBut.Click += new System.EventHandler(this.CancelBut_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(162, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.secLabel);
+            this.panel1.Controls.Add(this.minLabel);
+            this.panel1.Controls.Add(this.hoursLabel);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(155, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(430, 238);
+            this.panel1.TabIndex = 5;
+            // 
+            // secLabel
+            // 
+            this.secLabel.AutoSize = true;
+            this.secLabel.Location = new System.Drawing.Point(356, 122);
+            this.secLabel.Name = "secLabel";
+            this.secLabel.Size = new System.Drawing.Size(24, 13);
+            this.secLabel.TabIndex = 7;
+            this.secLabel.Text = "sec";
+            // 
+            // minLabel
+            // 
+            this.minLabel.AutoSize = true;
+            this.minLabel.Location = new System.Drawing.Point(198, 122);
+            this.minLabel.Name = "minLabel";
+            this.minLabel.Size = new System.Drawing.Size(23, 13);
+            this.minLabel.TabIndex = 6;
+            this.minLabel.Text = "min";
+            // 
+            // hoursLabel
+            // 
+            this.hoursLabel.AutoSize = true;
+            this.hoursLabel.Location = new System.Drawing.Point(55, 122);
+            this.hoursLabel.Name = "hoursLabel";
+            this.hoursLabel.Size = new System.Drawing.Size(13, 13);
+            this.hoursLabel.TabIndex = 5;
+            this.hoursLabel.Text = "h";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.slabel);
+            this.panel2.Controls.Add(this.mLabel);
+            this.panel2.Controls.Add(this.hLabel);
+            this.panel2.Location = new System.Drawing.Point(155, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(430, 238);
+            this.panel2.TabIndex = 6;
+            this.panel2.Visible = false;
+            // 
+            // hLabel
+            // 
+            this.hLabel.AutoSize = true;
+            this.hLabel.Location = new System.Drawing.Point(132, 109);
+            this.hLabel.Name = "hLabel";
+            this.hLabel.Size = new System.Drawing.Size(19, 13);
+            this.hLabel.TabIndex = 0;
+            this.hLabel.Text = "00";
+            // 
+            // mLabel
+            // 
+            this.mLabel.AutoSize = true;
+            this.mLabel.Location = new System.Drawing.Point(202, 109);
+            this.mLabel.Name = "mLabel";
+            this.mLabel.Size = new System.Drawing.Size(19, 13);
+            this.mLabel.TabIndex = 1;
+            this.mLabel.Text = "00";
+            // 
+            // slabel
+            // 
+            this.slabel.AutoSize = true;
+            this.slabel.Location = new System.Drawing.Point(264, 109);
+            this.slabel.Name = "slabel";
+            this.slabel.Size = new System.Drawing.Size(19, 13);
+            this.slabel.TabIndex = 2;
+            this.slabel.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ":";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ":";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(586, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "continue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CancelBut);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button CancelBut;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label secLabel;
+        private System.Windows.Forms.Label minLabel;
+        private System.Windows.Forms.Label hoursLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label slabel;
+        private System.Windows.Forms.Label mLabel;
+        private System.Windows.Forms.Label hLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ timer/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        int s, m, h;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (h == 0 && m == 0 && s == 0)
+            {
+                timer1.Stop();
+                panel2.Visible = false;
+                button1.Visible = true;
+                button2.Visible = false;
+                CancelBut.Enabled = false;
+                MessageBox.Show("Все!", "таймер все", MessageBoxButtons.OK);
+            }
+            s = s - 1;
+            if (s == -1)
+            {
+                m = m - 1;
+                s = 59;
+            }
+            if (m == -1)
+            {
+                h = h - 1;
+                m = 59;
+            }
+            if (h==0&&m==0&&s==0)
+            {
+                timer1.Stop();
+                panel2.Visible = false;
+                button1.Visible = true;
+                button2.Visible = false;
+                CancelBut.Enabled = false;
+                MessageBox.Show("Все!", "таймер все", MessageBoxButtons.OK);                
+            }
+            slabel.Text = Convert.ToString(s);
+            mLabel.Text = Convert.ToString(m);
+            hLabel.Text = Convert.ToString(h);
+        }
+
+        private void CancelBut_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            panel2.Visible = false;
+            button1.Visible = true;
+            button2.Visible = false;
+            CancelBut.Enabled = false;
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button2.Visible = false;
+            button3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button3.Visible = false;
+            button2.Visible = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CancelBut.Enabled = true;
+            s = Int32.Parse(textBox1.Text);
+            m = Int32.Parse(textBox2.Text);
+            h = Int32.Parse(textBox3.Text);
+            slabel.Text = Convert.ToString(s);
+            mLabel.Text = Convert.ToString(m);
+            hLabel.Text = Convert.ToString(h);
+            panel2.Visible = true;
+            button1.Visible = false;
+            button2.Visible = true;
+            timer1.Start();
+        }
+    }
+}
